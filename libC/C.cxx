be33c6e834d9729d1137fc8ac5b815900681de2b
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-11-13 22:55:07 +0100

    reduce the code to the strict minimum for a shared library

--- libC/C.cxx
+++ libC/C.cxx
@@ -1,21 +1,8 @@
 module;
-#include <godot_cpp/classes/sprite2d.hpp>
 #include <ostream>
 #include <stdexcept>
+#include <godot_cpp/classes/sprite2d.hpp>
 
 
 module C;
 
-using namespace std;
-
-
-namespace C
-{
-  void say_hello (ostream& o, const string& n)
-  {
-    if (n.empty ())
-      throw invalid_argument ("empty name");
-
-    o << "Hello, " << n << '!' << endl;
-  }
-}
