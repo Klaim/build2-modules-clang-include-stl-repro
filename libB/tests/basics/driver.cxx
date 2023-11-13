26d1712e8c485c243429e666b44b7e1db503c91a
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-11-13 17:44:56 +0100

    created libraries, no modules yet

---
+++ libB/tests/basics/driver.cxx
@@ -0,0 +1,35 @@
+#include <sstream>
+#include <stdexcept>
+
+#include <libB/version.hxx>
+#include <libB/B.hxx>
+
+#undef NDEBUG
+#include <cassert>
+
+int main ()
+{
+  using namespace std;
+  using namespace B;
+
+  // Basics.
+  //
+  {
+    ostringstream o;
+    say_hello (o, "World");
+    assert (o.str () == "Hello, World!\n");
+  }
+
+  // Empty name.
+  //
+  try
+  {
+    ostringstream o;
+    say_hello (o, "");
+    assert (false);
+  }
+  catch (const invalid_argument& e)
+  {
+    assert (e.what () == string ("empty name"));
+  }
+}
