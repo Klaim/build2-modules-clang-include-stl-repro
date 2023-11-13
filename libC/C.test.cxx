6fcf0dc6f7696d961c426b966c35020049ed9f00
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-11-13 21:11:31 +0100

    libC tests use doctest

--- libC/C.test.cxx
+++ libC/C.test.cxx
@@ -1,11 +1,33 @@
-#include <iostream>
-
 import C;
 
-#undef NDEBUG
-#include <cassert>
+#include <iostream>
 
-int main ()
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("basics")
 {
+    using namespace std;
+    using namespace C;
 
+    // Basics.
+    //
+    {
+        ostringstream o;
+        say_hello (o, "World");
+        CHECK (o.str () == "Hello, World!\n");
+    }
+
+    // Empty name.
+    //
+    try
+    {
+        ostringstream o;
+        say_hello (o, "");
+        CHECK (false);
+    }
+    catch (const invalid_argument& e)
+    {
+        CHECK (e.what () == string ("empty name"));
+    }
 }
